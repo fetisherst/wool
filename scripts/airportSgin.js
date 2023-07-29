660f14ab4383f02165bd5294f389899b7284f2d2
fetisherst <90710652+fetisherst@users.noreply.github.com>
2023-07-29 13:13:14 +0800

    Update airportSgin.js

--- scripts/airportSgin.js
+++ scripts/airportSgin.js
@@ -72,7 +72,7 @@ async function main() {
 // Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6
 // Cookie:
 //
-// {"token":"glados.network"}
+// {"token":"glados.one"}
 async function sign(cookie){
     return new Promise(resolve => {
         try {
