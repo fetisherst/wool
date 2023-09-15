981361286f1bd82c5603e96dcec446f63620d4ef
fetisherst <90710652+fetisherst@users.noreply.github.com>
2023-09-16 07:25:12 +0800

    Update shpj.js

--- shpj.js
+++ shpj.js
@@ -5,7 +5,7 @@
  变量：shpjCookie='xxxx@12345678910#abcdefg '  多个账号用 @ 或者 换行 分割
        jrychelpAu = true/false 用于是否助力作者(默认为true)
  定时一天三次
- cron: 30 8,10,19 * * *
+ cron: 38 8,10,19 * * *
  */
  const $ = new Env('诗画浦江')
  const notify = $.isNode() ? require('./sendNotify') : '';
